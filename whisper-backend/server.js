b0ae9aa102cbc98d6b069848270a87d0185cdf3c
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-04-03 23:49:38 +0530

    new changes

--- whisper-backend/server.js
+++ whisper-backend/server.js
@@ -1091,7 +1091,7 @@ ${transcriptionArray.map(t => t.value).join('\n')}`;
     }
 }
 
-
+// changes to be made here 
 app.post("/api/send-welcome-email", (req, res) => {
     const { email, userName } = req.body;
 
